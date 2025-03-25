7c21b93e813d0dd168083a6f8936af5feb9a4320
Ritabrata Patra <teamritabrata@gmail.com>
2025-03-25 15:34:34 +0530

    added changes to the dynamic url

--- app/[shorturl]/page.js
+++ app/[shorturl]/page.js
@@ -1,18 +1,29 @@
 import { redirect } from "next/navigation";
-
 import { dbConnect } from "@/lib/db/dbConnect";
-export default async function Page({ params }) {
-  const shortUrl = (await params).shorturl;
 
-  const client = await dbConnect();
-  const db = client.db("shortlink");
+let cachedClient = null; // Cache database connection
+
+async function getDb() {
+  if (!cachedClient) {
+    cachedClient = await dbConnect();
+  }
+  return cachedClient.db("shortlink");
+}
+
+export default async function Page({ params }) {
+  const shortUrl = params.shorturl;
+
+  const db = await getDb();
   const db_collection = db.collection("links");
 
-  const doc = await db_collection.findOne({ shortUrl: shortUrl });
+  const doc = await db_collection.findOne({ shortUrl });
+
   if (doc) {
     console.log(doc.url);
     redirect(doc.url);
+    return;
   } else {
     redirect(`${process.env.NEXT_PUBLIC_BASE_URL}`);
+    return;
   }
 }
